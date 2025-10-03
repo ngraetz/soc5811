eba2c940755893b9d6928007e1183f1d740a11fb
Nick Graetz <ncgraetz@gmail.com>
2025-10-03 11:09:40 -0400

    update

--- assignments/assignment_03.R
+++ assignments/assignment_03.R
@@ -8,32 +8,34 @@ library(tigris)     ## Functions for loading shapefiles directly from the Census
 library(sf)         ## Functions for shapefiles
 ###########################################################################################################
 
-psid <- readRDS("C:/Users/ngraetz/Dropbox/Penn/papers/psid/data/imputed_input_data_oct19.RDS")
-psid <- psid[[1]]
-psid <- psid[[1]][[1]]
-psid <- as.data.table(psid)
+## We will work with some data from the Panel Study of Income Dynamics (PSID), longest running longitudinal household survey in the world.
+## Beginning in 1968 with 5,000 families, the PSID has since followed individuals across generations and collects survey data every two years. 
+## In this cleaned version of some of the PSID variables, each row is a grandchild (g3_*) who we have attached to some variables from their
+## parents (g2_*) and their grandparents (g1_*). 
+psid <- fread('https://raw.githubusercontent.com/ngraetz/soc5811/refs/heads/main/data/psid.csv')
 
-write.csv(all_gens, 'C:/Users/ngraetz/Dropbox/Minnesota/repos/soc5811/data/psid.csv', row.names=F)
+## 1. Create an indicator variable with a character class (e.g., "more"/"less") for whether grandchildren had a 
+##    grandparent with a high school degree or less (g1_edu_cat).
 
-## 1. 
+## 2. Create a density plot of grandchildren home value percentile (g3_house_value_percentile). Plot two overlapping
+##    densities by assigning the "fill" aesthetic to your indicator variable above. Note that 0 = non-owner.  
+##    Hint: Increase the transparency of the densities using the "alpha" argument in geom_density() (e.g., alpha=0.4). 
 
-all_gens[, hs_or_less := ifelse(g1_edu_cat %in% c('less_hs','hs'),'less','more')]
-ggplot(data=all_gens,
-       aes(x=g3_house_value_percentile,
-           fill=hs_or_less)) + 
-  geom_density(alpha=0.2) + 
-  theme_bw()
+## 2. What is the average home value percentile of grandchildren whose parents had a high school degree or less?
+##    What about grandchildren whose parents had more than a high school degree?
 
-all_gens[, mean(g3_house_value_percentile), by='hs_or_less']
-  
+## 3. Is the difference between means above statistically significant? Use a linear regression model to estimate
+##    a p-value for the difference between these means and state your answer in terms of the null hypothesis. 
 
-summary(lm(g3_house_value_percentile~hs_or_less, data=all_gens))
+## 4. Fit a linear regression model predicting grandchild home value percentile (g3_house_value_percentile)
+##    as a function of grandparent home value percentile (g1_house_value_percentile). 
 
-summary(lm(g3_house_value_percentile~g1_house_value_quintile, data=all_gens))
+## 5. Interpret the coefficient on grandparent home value percentile.
 
+## 6. Is this coefficient estimate statistically significant at the p<0.05 level? What is the null hypothesis
+##    at which this p-value is being calculated?
 
-
-## 2. We'll soon begin thinking about the final project assignment, which will be due at the end of the semester.
+## 5. We'll soon begin thinking about the final project assignment, which will be due at the end of the semester.
 ##    I will post several datasets that can be used for this project, but you're also free to use any data you want.
 ##    To help me think about which datasets might be most relevant for you, please write 2-3 sentences about what sort
 ##    of topics you might be interested in for your final paper. Think about characterizing the relationship between
